35908af62ac830324fc0f866a15fa9ee227aeb96
Seth Battin <seth.battin@gmail.com>
2019-03-05 09:46:43 -0600

    implement webserver program

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cowsay = require("cowsay");
 
 app.use(express.static("content"));
+app.get("/cowsay", function(req, res){
+  const said = cowsay.say({text: "tell me what to say", ...req.query});
+  res.send(`<html><body><pre>${said}</pre></body></html>`);
+});
 
 app.listen(port, function (){ console.log("server is running!");});
