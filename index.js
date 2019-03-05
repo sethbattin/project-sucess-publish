a1abe0c60ade67fb8f7e2102b451a10d52f4c84c
Seth Battin <seth.battin@gmail.com>
2019-03-04 22:50:52 -0600

    make deployable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static("content"));
 
-app.listen(3000, function (){ console.log("server is running!");});
+app.listen(port, function (){ console.log("server is running!");});
